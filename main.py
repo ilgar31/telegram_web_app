689b626e6cacc539a1587c6cab0a4811f40f1e01
ilgar31 <ilgar.bagishev@gmail.com>
2024-01-25 17:58:23 +0300

    start project

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+
+bot = telebot.TeleBot('6958783112:AAEvgeKxpnClQFT1sT-kL7M-4R8-j0Z-1zI')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message == '\start':
+        bot.send_message(message.from_user.id, 'Привет', reply_markup=webAppKeyboard())
+    else:
+        bot.send_message(message.from_user.id, 'Воспользуйтесь меню', reply_markup=webAppKeyboard())
+
+
+def webAppKeyboard(): 
+   keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1)
+   webAppTest = telebot.types.WebAppInfo("https://bug-games.ru") 
+   one_butt = telebot.types.KeyboardButton(text="Games", web_app=webAppTest)
+   keyboard.add(one_butt)
+
+   return keyboard
+
+bot.polling(none_stop=True, interval=0)
